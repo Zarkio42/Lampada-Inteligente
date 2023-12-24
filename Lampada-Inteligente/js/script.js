6871878ab3890f074071909162af2741f9e733ba
zarkio42 <pedro.contato24@gmail.com>
2023-12-24 19:43:08 -0300

    subindo projeto da lampada

---
+++ Lampada-Inteligente/js/script.js
@@ -0,0 +1,79 @@
+const lampada = document.getElementById("img-lamp");
+var clickOnImage = 0;
+
+lampada.addEventListener("click", function () {
+    clickOnImage++
+    if (clickOnImage === 3) {
+        lampada.src = "./images/quebrada.png"
+        lampada.style = "width: 700px;"
+        alert("Você quebrou a lâmpada :(")
+        clickOnImage = 0
+    }
+})
+
+function trocaImg(v) {
+    let img = document.getElementById("img-lamp");
+    let detailLight = document.getElementById("detail-01")
+    if (v == 1) {
+        img.src = "./images/aceso3.png"
+        img.style = "width: 700px;"
+        detailLight.style = "background-color: green"
+    }
+    else {
+        img.src = "./images/apagado.png"
+        img.style = "width: 700px;"
+        detailLight.style = "background-color: red"
+    }
+}
+
+var speechOnOff = false;
+const listAcender = ["acender", "acender lâmpada", "acenda", "ligar", "ligar luz", "acender luz", "ligar lâmpada", "liga", "acende"];
+const listApagar = ["apagar", "apagar luz", "desligar", "apagar lâmpada", "desligar luz", "desligar lâmpada", "desliga", "apaga"];
+
+class speechApi {
+    constructor() {
+
+        this.speechApi = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+        this.speechApi.continuous = true;
+        this.speechApi.lang = "pt-BR";
+
+        this.speechApi.onresult = (e) => {
+            var resultIndex = e.resultIndex;
+            var transcript = e.results[resultIndex][0].transcript.toLowerCase();
+            console.log("Você disse:", transcript);
+
+            if ( listAcender.some(p => transcript.includes(p))) {
+                trocaImg(1);
+            } else if (listApagar.some(p => transcript.includes(p))) {
+                trocaImg(0);
+            }
+        };
+    }
+
+    start() {
+        this.speechApi.start();
+        speechOnOff = true;
+    }
+
+    stop() {
+        this.speechApi.stop();
+        speechOnOff = false;
+    }
+
+}
+
+const speechApiInstance = new speechApi();
+
+function startAndStop() {
+    if (speechOnOff) {
+        speechApiInstance.stop();
+    } else {
+        speechApiInstance.start();
+    }
+}
+
+
+
+
+
+
